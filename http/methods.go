0f3374d8c62b8a85c27ac4598d1d2b9bd4bd7ff2
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-08 12:34:51 +0530

    add logging for looped request

--- http/methods.go
+++ http/methods.go
@@ -1,8 +1,10 @@
 package http
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/abh1sheke/postx/parser"
 )
@@ -20,11 +22,15 @@ func Single(args *parser.Args, mutex *ResMutex) {
 func Looped(args *parser.Args, mutex *ResMutex) {
 	wg := new(sync.WaitGroup)
 	client := new(http.Client)
-    for {
-        for i := 1; i <= *args.Repeat; i++ {
-            wg.Add(1)
-            go makeRequest(i, client, args, mutex, wg)
-        } 
-        wg.Wait()
-    }
+	iterCount := 0
+	for {
+		for i := 1; i <= *args.Repeat; i++ {
+			wg.Add(1)
+			go makeRequest(i, client, args, mutex, wg)
+		}
+		iterCount++
+		fmt.Printf("%v iteration(s) done\n", iterCount)
+		time.Sleep(1 * time.Second)
+		wg.Wait()
+	}
 }
