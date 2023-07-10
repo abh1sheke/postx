63c145f695b1629b28c8b891a3de371fe8b565da
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-10 11:21:48 +0530

    implement logging

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/abh1sheke/postx/http"
+	"github.com/abh1sheke/postx/logging"
 	"github.com/abh1sheke/postx/parser"
 	"github.com/akamensky/argparse"
 )
@@ -22,6 +23,12 @@ func main() {
 		fmt.Print(argparser.Usage(err))
 		os.Exit(1)
 	}
+    logFile, logger, err := logging.InitLogging()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    defer logFile.Close()
 
 	resultMutex := http.InitResMutex(uint(*args.Repeat))
 
@@ -42,9 +49,9 @@ func main() {
 				}
 			}
 		}()
-		http.Looped(args, resultMutex)
+		http.Looped(args, resultMutex, logger)
 	} else {
-		http.Single(args, resultMutex)
+		http.Single(args, resultMutex, logger)
 	}
 
 	fmt.Printf(
