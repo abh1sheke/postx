47d97e5b7e8db51bf93c1e5780a1278050485d22
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-28 14:58:41 +0530

    add method to extract stringified header data

--- result/data.go
+++ result/data.go
@@ -1,6 +1,10 @@
 package result
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 type Data struct {
 	Body     *[]byte        `json:"data"`
@@ -8,10 +12,20 @@ type Data struct {
 }
 
 func (r *Data) GetData() *string {
-	str := string(*r.Body)
+	str := fmt.Sprintf("%v\n", string(*r.Body))
 	return &str
 }
 
-func (r *Data) GetResponse() *http.Response {
-	return r.Response
+func (r *Data) GetResponse() *string {
+	res := r.Response
+	resToStr := fmt.Sprintf("%s %s\n", res.Proto, res.Status)
+	for i, v := range res.Header {
+		var val = v[0]
+		if len(v) > 1 {
+			val = strings.Join(v, ",")
+		}
+		resToStr += fmt.Sprintf("%v: %v\n", i, val)
+	}
+	resToStr += "\n"
+	return &resToStr
 }
