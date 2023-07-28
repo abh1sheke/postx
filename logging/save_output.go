9075c518e1f34511238d1e7da9c34dec705402dc
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-28 14:56:33 +0530

    update function to only handle file writing

--- logging/save_output.go
+++ logging/save_output.go
@@ -4,33 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/abh1sheke/postx/http"
-	"github.com/goccy/go-json"
+	"path"
 )
 
-func SaveToFile(r *http.Result, outfile *string, logger *log.Logger) {
+func SaveToFile(r *string, outfile *string, logger *log.Logger) {
 	if len(*outfile) < 1 {
 		return
 	}
-	fmt.Println("saving output...")
-	output := make([]http.Res, len(*r.List))
-	for _, v := range *r.List {
-		if v != nil {
-			output = append(output, *v)
-		}
-	}
-
-	bytes, err := json.MarshalIndent(output, "", "  ")
-	if err != nil {
-		fmt.Println("could not write to outfile.")
-		fmt.Println(`check logs by running "cat $TMPDIR/postx.log".`)
-		logger.Printf("outfile write error: %v\n", err)
-		os.Exit(1)
-	}
-
-	err = os.WriteFile(
-		*outfile, bytes, 0644,
+	err := os.WriteFile(
+		*outfile, []byte(*r), 0644,
 	)
 	if err != nil {
 		fmt.Println("could not write to outfile.")
@@ -38,5 +20,5 @@ func SaveToFile(r *http.Result, outfile *string, logger *log.Logger) {
 		logger.Printf("outfile write error: %v\n", err)
 		os.Exit(1)
 	}
-	fmt.Printf("wrote output to %v.\n", *outfile)
+	fmt.Printf("postx: wrote outfile to %v.\n", path.Base(*outfile))
 }
