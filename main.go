1dec8a89222b31ef93ea9b4b7a41c01cf3a888f6
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-08-11 17:27:55 +0530

    use cobra for arg parsing

--- main.go
+++ main.go
@@ -3,35 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 
-	"github.com/abh1sheke/postx/logging"
-	"github.com/abh1sheke/postx/parser"
-	"github.com/abh1sheke/postx/runners"
-	"github.com/akamensky/argparse"
+	"github.com/abh1sheke/postx/cmd"
 )
 
 func main() {
-	argparser := argparse.NewParser(
-		"postx",
-		"A CLI tool for transferring data over HTTP.",
-	)
-	args, err := parser.Build(argparser)
-	if err != nil {
-		fmt.Print(argparser.Usage(err))
-		os.Exit(1)
-	}
-	logFile, logger, err := logging.InitLogging()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer logFile.Close()
-
-	startTime := time.Now()
-	if *args.Loop {
-		runners.Looped(args, startTime, logger)
-	} else {
-		runners.Single(args, logger)
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Error: %s", err.Error()))
 	}
 }
