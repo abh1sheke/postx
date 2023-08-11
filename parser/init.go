71bdfcd40c938d53e5553118c445a81d558b4b7a
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-08-11 18:47:53 +0530

    add numerical field

--- parser/init.go
+++ parser/init.go
@@ -1,14 +1,15 @@
 package parser
 
 type Args struct {
-	Method   *string
-	URL      *string
-	Data     *[]string
-	Headers  *[]string
-	Parallel *int
-	Loop     *bool
-	Files    *[]string
-	Include  *bool
-	Time     *bool
-	Output   *string
+	Method    *string
+	URL       *string
+	Data      *[]string
+	Numerical *[]string
+	Headers   *[]string
+	Parallel  *int
+	Loop      *bool
+	Files     *[]string
+	Include   *bool
+	Time      *bool
+	Output    *string
 }
