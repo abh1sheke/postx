18998d39dc88d73b59df37fef0f958bc1a5f7b97
Abhiseke <abhisheke.acharya@gmail.com>
2023-11-14 16:56:21 +0530

    fix single runner test

--- runners/single_test.go
+++ runners/single_test.go
@@ -1,11 +1,8 @@
 package runners
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
-	"github.com/abh1sheke/postx/logging"
 	"github.com/abh1sheke/postx/parser"
 )
 
@@ -20,13 +17,7 @@ func BenchmarkSingle(t *testing.B) {
 		Output:  &output,
 		Headers: &headers,
 	}
-	logFile, logger, err := logging.InitLogging()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer logFile.Close()
 	args.Parallel = &t.N
 
-	Single(&args, logger)
+	Single(&args)
 }
