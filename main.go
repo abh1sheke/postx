f4adc9b2630a4f98f5f413dbd53e2f43347b04b5
Abhisheke <abhisheke.acharya@gmail.com>
2023-12-29 18:22:23 +0530

    implement module print

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/abh1sheke/postx/cmd"
-	"github.com/abh1sheke/postx/logging"
+	"github.com/abh1sheke/postx/print"
 )
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		logging.EPrintf("Error: %s", err.Error())
+		print.Efatalf("Error: %v", err)
 	}
 }
