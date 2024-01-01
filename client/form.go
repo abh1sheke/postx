5854fd20059bc8cb5466f19351aa41c4f67904c5
Abhisheke <abhisheke.acharya@gmail.com>
2024-01-01 13:32:06 +0530

    feat: create module client for sending http requests

---
+++ client/form.go
@@ -0,0 +1,47 @@
+package client
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func constructMutlipart(files, data map[string]string) (io.Reader, string, error) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	for param, file := range files {
+		b, err := os.ReadFile(file)
+		if err != nil {
+			return nil, "", err
+		}
+		part, err := writer.CreateFormFile(param, filepath.Base(file))
+		if err != nil {
+			return nil, "", err
+		}
+		if _, err = part.Write(b); err != nil {
+			return nil, "", err
+		}
+	}
+	for k, v := range data {
+		if err := writer.WriteField(k, v); err != nil {
+			return nil, "", err
+		}
+	}
+	if err := writer.Close(); err != nil {
+		return nil, "", err
+	}
+	return body, writer.FormDataContentType(), nil
+}
+
+func contructURLEncoded(data map[string]string) (io.Reader, int, error) {
+	form := new(url.Values)
+	for k, v := range data {
+		form.Set(k, v)
+	}
+	encoded := form.Encode()
+	return strings.NewReader(encoded), len(encoded), nil
+}
---
+++ client/request.go
@@ -0,0 +1,42 @@
+package client
+
+import (
+	"io"
+	"net/http"
+	"reflect"
+	"strconv"
+
+	"github.com/abh1sheke/postx/args"
+)
+
+func buildRequest(a *args.Args) (*http.Request, error) {
+	var body io.Reader
+	var size int
+	var err error
+	var multipartHeader string
+	if len(a.Files) > 0 || a.Multi {
+		body, multipartHeader, err = constructMutlipart(a.Files, a.Data)
+	} else if len(a.Files) == 0 && len(a.Data) > 0 {
+		body, size, err = contructURLEncoded(a.Data)
+	}
+	if err != nil {
+		return nil, err
+	}
+	r, err := http.NewRequest(a.Method, a.URL, body)
+	if err != nil {
+		return nil, err
+	}
+	if body != nil {
+		switch reflect.TypeOf(body).String() {
+		case "*bytes.Buffer":
+			r.Header.Add("Content-Type", multipartHeader)
+		case "*strings.Reader":
+			r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			r.Header.Add("Content-Length", strconv.Itoa(size))
+		}
+	}
+	for k, v := range a.Headers {
+		r.Header.Add(k, v)
+	}
+	return r, nil
+}
