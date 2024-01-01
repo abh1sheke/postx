5d03b9969b6194b8bebecec2c29eef8169f003dd
Abhisheke <abhisheke.acharya@gmail.com>
2024-01-01 10:42:25 +0530

    fix: propagate errors to caller

--- args/args.go
+++ args/args.go
@@ -1,10 +1,11 @@
 package args
 
 import (
-	"strings"
 	"time"
 )
 
+// Args holds all the necessary information to perfrom a HTTP request
+// as specified by the user
 type Args struct {
 	Method, Output, URL, Proxy string
 	Data, Files, Headers       map[string]string
@@ -12,16 +13,12 @@ type Args struct {
 	Timeout                    time.Duration
 }
 
-func (a *Args) GetMethod() string {
-	return strings.ToUpper(a.Method)
-}
-
 func (a *Args) Verify() error {
 	if err := verifyMethod(a.Method); err != nil {
-		return nil
+		return err 
 	}
 	if err := verifyURL(a.URL); err != nil {
-		return nil
+		return err 
 	}
 	return nil
 }
