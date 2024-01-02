d28d1ac720849127dff0a1aca81dd8af19487b25
Abhisheke <abhisheke.acharya@gmail.com>
2024-01-02 14:18:22 +0530

    feat: update help and add more options for data transfer

--- cmd/root.go
+++ cmd/root.go
@@ -9,20 +9,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const VERSION = "0.1.0"
+const (
+	VERSION    = "0.1.0"
+	USER_AGENT = "zing/" + VERSION
+)
 
 var _args *args.Args
-var method, output, url, proxy, json string
-var files, data, headers []string
+var method, output, url, proxy, dataText, json, userAgent string
+var files, data, dataForm, headers []string
 var multi, include bool
 var timeout int64
 
 var rootCmd = &cobra.Command{
-	Use:   "zing",
-	Short: "A fast and feature-rich alternative to cURL.",
-	// SilenceErrors: true,
-	// SilenceUsage:  true,
-	RunE: func(cmd *cobra.Command, _ []string) error {
+	Use:   "zing <url> [flags]",
+	Short: "A cross-platform, fast and easy-to-use HTTP client for the command-line.",
+	RunE: func(cmd *cobra.Command, a []string) error {
+		if len(a) < 1 {
+			return fmt.Errorf("no URL specified!")
+		} else {
+			url = a[0]
+		}
 		var _data, _files, _headers map[string]string
 		var err error
 		if _data, err = args.ParseKV(data, "data"); err != nil {
@@ -65,16 +71,16 @@ func init() {
 	rootCmd.Version = VERSION
 
 	rootCmd.Flags().StringVarP(&method, "method", "m", "get", "http request method")
+	rootCmd.Flags().StringVarP(&userAgent, "user-agent", "A", USER_AGENT, "specify User-Agent to send")
 	rootCmd.Flags().StringVarP(&output, "output", "o", "", "specify output file")
-	rootCmd.Flags().StringVarP(&url, "url", "u", "", "endpoint url")
 	rootCmd.Flags().StringVarP(&proxy, "proxy", "p", "", "proxy url")
-	rootCmd.Flags().StringVar(&json, "json", "", "http POST data as JSON")
+	rootCmd.Flags().StringVar(&dataText, "data-text", "", "set http POST data as text/plain")
+	rootCmd.Flags().StringVarP(&json, "data-json", "j", "", "set http POST data as JSON")
 	rootCmd.Flags().BoolVarP(&multi, "multipart", "M", false, "send request data as multipart/form-data")
-	rootCmd.Flags().StringArrayVarP(&data, "data", "d", []string{}, "set http POST data (name=value)")
-	rootCmd.Flags().StringArrayVarP(&files, "file", "F", []string{}, "set MIME multipart MIME file (name=data)")
+	rootCmd.Flags().StringArrayVarP(&data, "data", "d", []string{}, "set http POST data")
+	rootCmd.Flags().StringArrayVarP(&files, "file", "F", []string{}, "set MIME multipart MIME file (name=file)")
+	rootCmd.Flags().StringArrayVar(&dataForm, "data-form", []string{}, "set http POST data as multipart/form-data (name=value)")
 	rootCmd.Flags().StringArrayVarP(&headers, "headers", "H", []string{}, "set request headers (name=value)")
 	rootCmd.Flags().Int64VarP(&timeout, "timeout", "t", 10, "set request timeout (in seconds)")
 	rootCmd.Flags().BoolVarP(&include, "include", "i", false, "include request headers in output")
-
-	rootCmd.MarkFlagRequired("url")
 }
